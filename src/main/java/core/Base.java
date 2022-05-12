a1f12bc248d1dc6d7bc0f0c1fcc03d6997dee452
DawoodKazemi <mohammad.dawood@tekschool.us>
2022-05-12 23:30:25 +0100

    all files added to the repo

---
+++ src/main/java/core/Base.java
@@ -0,0 +1,42 @@
+package core;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import org.apache.log4j.Logger;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.openqa.selenium.WebDriver;
+
+//import io.cucumber.core.logging.Logger;
+
+public class Base {
+	
+	public static Properties properties;
+	public static WebDriver driver;
+	public static Logger logger;
+	private String configPath =".\\src\\trest\\resources\\input\\config.properties";
+	private String log4jPath= ".\\src\\trest\\resources\\input\\log4j.properties";
+	
+	public Base() throws FileNotFoundException {
+		try {
+			BufferedReader reader = new BufferedReader (new FileReader(configPath));
+			properties = new Properties();
+			
+				properties.load(reader);
+				reader.close();
+	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		logger = Logger.getLogger(log4jPath);
+		PropertyConfigurator.configure(log4jPath);	
+	}
+
+	
+
+
+}
